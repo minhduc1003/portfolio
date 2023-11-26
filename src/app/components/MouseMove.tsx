45619bc44df3aaac1d621d8e8958f1712d94a9de
minh duc <vuminhduc231003@gmail.com>
2023-11-26 10:06:15 +0700

    new: add background feature when move mouse

---
+++ src/app/components/MouseMove.tsx
@@ -0,0 +1,24 @@
+"use client";
+import React, { useEffect, useState } from "react";
+export default function MouseMove() {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e: any) => {
+    setMousePosition({ x: e.clientX, y: e.clientY });
+  };
+  useEffect(() => {
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+  return (
+    <div
+      className="pointer"
+      style={{
+        background: `radial-gradient(600px at ${mousePosition.x}px ${mousePosition.y}px, rgba(29, 78, 216, 0.15), transparent 80%)`,
+      }}
+    ></div>
+  );
+}
