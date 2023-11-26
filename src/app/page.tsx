7e3e9b85e6674e917dde1d054716f25dcda17a6d
minh duc <vuminhduc231003@gmail.com>
2023-11-26 14:58:24 +0700

    fix: error deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,20 +35,20 @@ export default function Home() {
           <p>
             Back in 2012, I decided to try my hand at creating custom Tumblr
             themes and tumbled head first into the rabbit hole of coding and web
-            development. Fast-forward to today, and I’ve had the privilege of
-            building software for an advertising agency, a start-up, a
+            development. Fast-forward to today, and I&apos;ve had the privilege
+            of building software for an advertising agency, a start-up, a
             student-led design studio, and a huge corporation.
           </p>
           <p>
             My main focus these days is building products and leading projects
-            for our clients at Upstatement. In my free time I've also released
-            an online video course that covers everything you need to know to
-            build a web app with the Spotify API.
+            for our clients at Upstatement. In my free time I&apos;ve also
+            released an online video course that covers everything you need to
+            know to build a web app with the Spotify API.
           </p>
           <p>
-            When I’m not at the computer, I’m usually rock climbing, hanging out
-            with my wife and two cats, or running around Hyrule searching for
-            Korok seeds K o r o k s e e d s .
+            When I&apos;m not at the computer, I&apos;m usually rock climbing,
+            hanging out with my wife and two cats, or running around Hyrule
+            searching for Korok seeds K o r o k s e e d s .
           </p>
         </div>
       </aside>
